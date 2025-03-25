aa05e3e52b0da0631d79c6a48e7e2f9ce1aad1ca
SNUChanKim <chan_kim@snu.ac.kr>
2025-03-25 11:46:24 +0900

    training script for sac-env and sac-zero in Humanoid environment added

---
+++ scripts/Humanoid/OOD/train_sac_env.sh
@@ -0,0 +1,5 @@
+for seed in 1 2 3 4 5
+do
+python run/retraining.py --env-name HumanoidOOD-v3 --observation-type vector --cuda --use-env-reward \
+	--cuda-device 0 --num-steps 4000001 --policy sac --buffer-size 1000000 --start-steps 10000 --seed $seed --load-model --consol-coef 0 --async-env --n-envs 5
+done
